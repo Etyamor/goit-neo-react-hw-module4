d6d1179d2102022d4f022e1388ad0793027ddeb4
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-04-27 13:21:37 +0300

    Update unsplash-api.js

--- src/unsplash-api.js
+++ src/unsplash-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiKey = "Ip5A1MvTpuHHH5z2J3qUqeuYMpGW--rQeu7x6R-KQs4";
-axios.defaults.baseURL = "https://api.u123nsplash.com";
+axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const searchImages = async (query, page = 1, perPage = 10) => {
   return await axios.get("/search/photos", {
